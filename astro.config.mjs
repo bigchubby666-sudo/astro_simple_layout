e4367fbcb9f5591b8c090250c8aeecce3080451e
Roy Andreson <bigchubby666@gmail.com>
2025-12-26 01:56:19 -0800

    fix: require site option

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://example.com",
+  site: "https://example.com",
   output: "static",
   integrations: [preact()],
   adapter: netlify()
